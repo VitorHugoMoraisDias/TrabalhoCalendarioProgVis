aaa395deecd3b0ebc3529780f2a692c236f88f58
VitorHugoMoraisDias <65238320+VitorHugoMoraisDias@users.noreply.github.com>
2022-03-27 12:43:06 -0300

    trabalho

---
+++ Calendario.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1p
+{
+    public class Calendario
+    {
+        public Byte Mes { get; set; }
+        public UInt16 Ano { get; set; }
+
+        //Verificar o mês com os valores máximos e mínimos fornecidos pelo DateTime, e o auxílio de um Boolean :
+        public bool isMesValido()
+        {
+            return ((Mes >= DateTime.MinValue.Month) && (Mes <= DateTime.MaxValue.Month));
+        }
+
+        // Verificar o ano, utilizando os mesmos métodos efetuados no mês, porém, trocando o método DateTime de 'Month' para 'Year' :
+        public bool isAnoValido()
+        {
+            return ((Ano >= DateTime.MinValue.Year) && (Ano <= DateTime.MaxValue.Year));
+        }
+        //gerar calendário
+        public string GerarCalendario()
+        {
+
+            // para a construção do calendário, será necessário criar um modelo:
+            // criação do cabeçalho utilizando um dia fornecido manualmente '1', para o funcionamento do mesmo.
+            DateTime calendario = new DateTime(Ano, Mes, 1);
+            string molde = $"{calendario.ToString("MMMM")} de {calendario.Year}";
+            //Criação do modelo de referência para a segunda parte do cabeçalho
+            string result = String.Format("\n\n" + molde.PadLeft(17 + molde.Length / 7, ' ') + "\n");
+
+            Console.WriteLine("╔═══════════════════════════════════════════════════════════╗\n");
+            Console.WriteLine("║---[D]-----[S]------[T]------[Q]------[Q]------[S]-----[S] ║ \n");
+            Console.WriteLine("╚═══════════════════════════════════════════════════════════╝\n");
+
+            //Método 'For' que irá gerar um loop para os dias da semana que não irão conter o dia '1' no início do calendário:
+            Int16 comparador = Convert.ToInt16(DateTime.DaysInMonth(Ano, Mes));
+            for (int i = 0; i < (int)calendario.DayOfWeek; i++)
+                Console.Write("         ");
+            for (int i = 1; i <= comparador; i++)
+            {
+                DateTime g = new DateTime(Ano, Mes, i);
+                if (g.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    if (i < 10)
+                        Console.Write("    " + i + "    ");
+                    else
+                        Console.Write("   " + i + "    ");
+                }
+
+                if (g.DayOfWeek == DayOfWeek.Monday)
+                {
+                    if (i < 10)
+                        Console.Write("    " + i + "    ");
+                    else
+                        Console.Write("   " + i + "    ");
+                }
+
+                if (g.DayOfWeek == DayOfWeek.Tuesday)
+                {
+                    if (i < 10)
+                        Console.Write("    " + i + "    ");
+                    else
+                        Console.Write("   " + i + "    ");
+                }
+
+                if (g.DayOfWeek == DayOfWeek.Wednesday)
+                {
+                    if (i < 10)
+                        Console.Write("    " + i + "    ");
+                    else
+                        Console.Write("   " + i + "    ");
+                }
+
+                if (g.DayOfWeek == DayOfWeek.Thursday)
+                {
+                    if (i < 10)
+                        Console.Write("    " + i + "    ");
+                    else
+                        Console.Write("   " + i + "    ");
+                }
+
+                if (g.DayOfWeek == DayOfWeek.Friday)
+                {
+                    if (i < 10)
+                        Console.Write("    " + i + "    ");
+                    else
+                        Console.Write("   " + i + "    ");
+                }
+
+
+                if (g.DayOfWeek == DayOfWeek.Saturday)
+                {
+                    if (i < 10)
+                        Console.WriteLine("    " + i);
+                    else
+                        Console.WriteLine("   " + i);
+                }
+            }
+            Console.WriteLine("\n═══════════════════════════════════════════════════════════\n");
+            return result;
+        }
+    }
+}
+    
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+
+namespace ConsoleApp1p
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calendario c = new Calendario();
+            //Início
+            Console.WriteLine("╔═══════════════════════════════════════════════════════╗");
+            Console.WriteLine("║      Bem vindo ào calendário automático!!!            ║");
+            Console.WriteLine("╠═══════════════════════════════════════════════════════╣");
+            Console.WriteLine("║  - Inserindo apenas o ano e o mês desejados, você     ║");
+            Console.WriteLine("║ poderá descobrir quantos são os dias do mês, e como   ║");
+            Console.WriteLine("║ estão distribuídos!                                   ║");
+            Console.WriteLine("╠═══════════════════════════════════════════════════════╣");
+            Console.WriteLine("║          [Aperte Enter para continuar...]             ║");
+            Console.WriteLine("╚═══════════════════════════════════════════════════════╝");
+            Console.ReadLine();
+            // Inserção do Ano, se o Ano estiver errado, será iniciado um loop através do '!c.isAnoValido()' 
+            while (!c.isAnoValido())
+            {
+                short comparador = 0;//Criação de um comparador para o método 'TryParse'
+                Console.WriteLine("╔═══════════════════════════════════════════════════════╗");
+                Console.WriteLine("║ »Por favor, insira um ano, no calendário gregoriano:  ║");
+                Console.WriteLine("╚═══════════════════════════════════════════════════════╝");
+
+                Int16.TryParse(Console.ReadLine(), out comparador);// Comparação padrão com o método 'TryParse'
+                c.Ano = Convert.ToUInt16(comparador);
+                //Caso 'c.isAnoValido()' não retorne um true....
+                if (!c.isAnoValido())
+                {
+                    Console.WriteLine("\n╔═════════════════════════════════════════════════════════════════╗");
+                    Console.WriteLine("║         »Por favor, insira um valor válido para o ano...        ║");
+                    Console.WriteLine("╚═════════════════════════════════════════════════════════════════╝\n");
+                }
+                    //e se reinicia o loop
+            }
+
+                //Inserção do Mês
+                while (!c.isMesValido())
+            {
+                short comparador = 0;//Definição do método comparador para 0, pra que assim ocorra o funcionamento do loop;
+                Console.WriteLine("╔═══════════════════════════════════════════════════════╗");
+                Console.WriteLine("║      [Insira um valor que corresponde a um Mês]       ║");
+                Console.WriteLine("╠════════════╦═══════════════╦══════════════╦═══════════╣");
+                Console.WriteLine("║1-Janeiro   | 2-Fevereiro   | 3-Março      | 4-Abril   ║");
+                Console.WriteLine("║5-Maio      ║ 6-Junho       ║ 7-Julho      ║ 8-Agosto  ║");
+                Console.WriteLine("║9-Setembro  |10-Outubro     |11-Novembro   |12-Dezembro║");
+                Console.WriteLine("╚════════════╩═══════════════╩══════════════╩═══════════╝");
+
+                Int16.TryParse(Console.ReadLine(), out comparador);// Comparação padrão com o método 'TryParse'
+                c.Mes = Convert.ToByte(comparador);
+                //Caso 'c.isMesValido()' não retorne um true....
+                if (!c.isMesValido())
+                Console.WriteLine("\n╔═════════════════════════════════════════════════════════════════╗");
+                Console.WriteLine("║         »Por favor, insira um valor válido para o mês...        ║");
+                Console.WriteLine("╚═════════════════════════════════════════════════════════════════╝\n");
+                //e se reinicia o loop
+            }
+            Console.Clear();
+                Console.WriteLine(c.GerarCalendario());
+
+                                Console.WriteLine("\n╔═════════════════════════════════════════════════════════════╗");
+                                Console.WriteLine("║    »Aperte Enter para ver o nome completo da equipe:        ║");
+                                Console.WriteLine("╚═════════════════════════════════════════════════════════════╝");
+                                Console.ReadLine();
+                                Console.Clear();
+                                Console.WriteLine("╔════════════════════════════════╗");
+                                Console.WriteLine("║♠           Equipe:            ♠║");
+                                Console.WriteLine("╠════════════════════════════════╣");
+                                Console.WriteLine("║ -Ruth Nunes de Oliveira        ║");
+                                Console.WriteLine("║ -Maria Beatriz Lopes Santiago  ║");
+                                Console.WriteLine("║ -Vitor Hugo Morais Dias        ║");
+                                Console.WriteLine("╚════════════════════════════════╝");
+                                Console.ReadLine();
+                    
+                
+            
+        }
+    }
+}
